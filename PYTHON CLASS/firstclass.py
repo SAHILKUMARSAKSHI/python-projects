27295ee60c081c04977b95a8afd64fa79fb5333c
SAHILKUMARSAKSHI <sahilkumarsakshi_ec22a18_12@dtu.ac.in>
2024-04-09 05:24:51 +0530

    go

---
+++ PYTHON CLASS/firstclass.py
@@ -0,0 +1,34 @@
+# print("hello world")
+# print('hello world')	
+# a = 2
+# b = 3.5
+# print(a+b)
+
+# write a program to add two numbers taking input from user
+
+# first = int(input("enter your first number  : "))
+# second = int(input("enter your second number : "))
+
+# print ("sum of the given numbers id : ", first + second)
+
+# write a programe to input side of a square and print its area.
+
+# side = float(input("enter the lenght of side :"))
+# print ("the are of the square is : ", side * side)
+
+
+# print the avg of a two numbers
+
+# a = float(input("enter your first number : "))
+# b = float(input("enter your second number : "))
+
+# print ("the avg of the given numbers is", (a+b)/2)
+
+# print true if value of a is greater and equals than b else print false
+
+# a = int(input("enter your first number :"))
+# b = int(input("enter your second number : "))
+
+# print( a>=b)
+
+
